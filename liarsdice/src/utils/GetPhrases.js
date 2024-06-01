33e92cda5e4a3a26457b8487ca90f3ab6b1d70d3
Roger Tan <teochewthunder@gmail.com>
2024-06-01 22:54:19 +0800

    Quit, Restart, Re-indexing.

--- liarsdice/src/utils/GetPhrases.js
+++ liarsdice/src/utils/GetPhrases.js
@@ -1,25 +1,25 @@
 const GetPhrases = (personality, phraseName, lang) => {
 	let labels = [
-		{ personality: 0, phraseName: "intro", lang: "en", value: "Hello, I'm Little Grass. Please go easy on me!"},
-		{ personality: 0, phraseName: "intro", lang: "cn", value: ""},
-		{ personality: 0, phraseName: "intro", lang: "en", value: "Hello, I'm Little Grass. I'm new at this. Shall we begin?"},
-		{ personality: 0, phraseName: "intro", lang: "cn", value: ""},
-		{ personality: 1, phraseName: "intro", lang: "en", value: "My name is Chanel. Let's play!"},
+		{ personality: 1, phraseName: "intro", lang: "en", value: "Hello, I'm Little Grass. Please go easy on me!"},
 		{ personality: 1, phraseName: "intro", lang: "cn", value: ""},
-		{ personality: 1, phraseName: "intro", lang: "en", value: "I'm Chanel. Are you familiar with this game?"},
+		{ personality: 1, phraseName: "intro", lang: "en", value: "Hello, I'm Little Grass. I'm new at this. Shall we begin?"},
 		{ personality: 1, phraseName: "intro", lang: "cn", value: ""},
-		{ personality: 2, phraseName: "intro", lang: "cn", value: "Hi handsome! Please call me Lulu. Shall we begin?"},
+		{ personality: 2, phraseName: "intro", lang: "en", value: "My name is Chanel. Let's play!"},
 		{ personality: 2, phraseName: "intro", lang: "cn", value: ""},
-		{ personality: 2, phraseName: "intro", lang: "cn", value: "How are you, handsome? I'm Lulu. Let's play!"},
+		{ personality: 2, phraseName: "intro", lang: "en", value: "I'm Chanel. Are you familiar with this game?"},
 		{ personality: 2, phraseName: "intro", lang: "cn", value: ""},
-		{ personality: 3, phraseName: "intro", lang: "cn", value: "How do you do? I'm happy to meet you. I am Gongsun Lan."},
+		{ personality: 3, phraseName: "intro", lang: "cn", value: "Hi handsome! Please call me Lulu. Shall we begin?"},
 		{ personality: 3, phraseName: "intro", lang: "cn", value: ""},
-		{ personality: 3, phraseName: "intro", lang: "cn", value: "Welcome! I am Gongsun Lan, and I will be your opponent this round."},
+		{ personality: 3, phraseName: "intro", lang: "cn", value: "How are you, handsome? I'm Lulu. Let's play!"},
 		{ personality: 3, phraseName: "intro", lang: "cn", value: ""},
-		{ personality: 4, phraseName: "intro", lang: "cn", value: "Hey, I'm Big Sister Spring. Are you here to play?"},
+		{ personality: 4, phraseName: "intro", lang: "cn", value: "How do you do? I'm happy to meet you. I am Gongsun Lan."},
 		{ personality: 4, phraseName: "intro", lang: "cn", value: ""},
-		{ personality: 4, phraseName: "intro", lang: "cn", value: "I'm the famous Big Sister Spring. You want to play? Let's go!"},
-		{ personality: 4, phraseName: "intro", lang: "cn", value: ""}
+		{ personality: 4, phraseName: "intro", lang: "cn", value: "Welcome! I am Gongsun Lan, and I will be your opponent this round."},
+		{ personality: 4, phraseName: "intro", lang: "cn", value: ""},
+		{ personality: 5, phraseName: "intro", lang: "cn", value: "Hey, I'm Big Sister Spring. Are you here to play?"},
+		{ personality: 5, phraseName: "intro", lang: "cn", value: ""},
+		{ personality: 5, phraseName: "intro", lang: "cn", value: "I'm the famous Big Sister Spring. You want to play? Let's go!"},
+		{ personality: 5, phraseName: "intro", lang: "cn", value: ""}
 	];
 
 	let match = phrases.filter((x)=> { return (x.phraseName == phraseName && x.lang == lang && (x.personality == null || x.personality == personality)); });
