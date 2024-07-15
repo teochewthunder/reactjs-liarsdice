94f796a8c9b461d0f4fc7304083177360bcb4b0a
Roger Tan <teochewthunder@gmail.com>
2024-07-15 18:04:11 +0800

    Dice tests

--- liarsdice/src/components/Dice/Dice.js
+++ liarsdice/src/components/Dice/Dice.js
@@ -15,7 +15,7 @@ function Dice(props) {
     	[0,0,0,0,0,0,0,0,0].map(function(dot, dotIndex){
     		var css = (show ? "dot val" + GetDiceDots(dice, dotIndex) : "dot hideDice");
 
-			return <div className={ css } key={ classPrefix + diceIndex + "_" + dotIndex }>
+			return <div className={ css } title={ css } key={ classPrefix + diceIndex + "_" + dotIndex }>
 
 			</div>
 		})
---
+++ liarsdice/src/components/Dice/Dice.test.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Dice from "./Dice";
+
+import GetDiceDots from "../../utils/GetDiceDots";
+
+describe("Dice", () => {
+  it("renders dice for 6", () => {
+    render(
+      <Dice
+        dice = "6"
+        diceIndex = "0"
+        classPrefix = "opponentDice"
+        highlight = { false }
+        show = { true }
+      />
+    );
+
+    expect(screen.queryAllByTitle("dot val1").length).toBe(6);
+  });
+});
+
+describe("Dice", () => {
+  it("renders dice for 1", () => {
+    render(
+      <Dice
+        dice = "1"
+        diceIndex = "0"
+        classPrefix = "opponentDice"
+        highlight = { false }
+        show = { true }
+      />
+    );
+
+    expect(screen.queryAllByTitle("dot val1").length).toBe(1);
+  });
+});
+
+describe("Dice", () => {
+  it("renders dice for 1 but not revealed", () => {
+    render(
+      <Dice
+        dice = "1"
+        diceIndex = "0"
+        classPrefix = "opponentDice"
+        highlight = { false }
+        show = { false }
+      />
+    );
+
+    expect(screen.queryAllByTitle("dot val1").length).toBe(0);
+  });
+});
+
